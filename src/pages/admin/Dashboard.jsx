2caba8a24a55f9ad8280dfb393a0278e210add9a
Anubiz999 <anuchit.nu@gmail.com>
2025-08-29 21:22:58 +0700

    🔧 Fix import path for axiosInstance in Dashboard.jsx

--- src/pages/admin/Dashboard.jsx
+++ src/pages/admin/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import axios from "../utils/axiosInstance";
+import axios from "../../../utils/axiosInstance";
 import useEcomStore from "../../store/ecom-store";
 import { DisplayResults } from "../../components/admin/Dashboard/DisplayResults";
 import DashboardAlert from "../../components/alert/DashboardAlert";
