a8c5ede6189d073df7aebcb6e372bffe681e6e12
Anubiz999 <anuchit.nu@gmail.com>
2025-09-30 23:10:17 +0700

    edit

--- src/pages/user/PaymentPage.jsx
+++ src/pages/user/PaymentPage.jsx
@@ -125,7 +125,7 @@ const PaymentPage = () => {
                         className={`px-4 py-2 rounded font-medium ${method === m ? "bg-blue-600 text-white" : "bg-gray-200"}`}
                     >
                         {m === "PROMPTPAY" && "พร้อมเพย์"}
-                        {m === "QR" && "แสกน QR Code"}
+                        {m === "QR" && "สแกน QR Code"}
                         {m === "CASH" && "เงินสด"}
                     </button>
                 ))}
